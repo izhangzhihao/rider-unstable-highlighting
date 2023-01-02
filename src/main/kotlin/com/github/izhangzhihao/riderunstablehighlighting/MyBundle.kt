e986ef39f6960b558c70a11b0db712977ca87db0
GitHub Action <action@github.com>
2023-01-02 10:43:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/izhangzhihao/riderunstablehighlighting/MyBundle.kt
+++ src/main/kotlin/com/github/izhangzhihao/riderunstablehighlighting/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.izhangzhihao.riderunstablehighlighting
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/izhangzhihao/riderunstablehighlighting/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/izhangzhihao/riderunstablehighlighting/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.izhangzhihao.riderunstablehighlighting.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.izhangzhihao.riderunstablehighlighting.MyBundle
 
 class MyApplicationService {
 
